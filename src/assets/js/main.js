afd072e91c0146c41cdfffe32c35e0eb3c1b7df4
BluePraise <email@magaliechetrit.com>
2023-12-01 20:27:58 -0500

    Slider
    - slider functionality with Swiper.js
    - Example images
    - Markup in NunJucks Template

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', (event) => {
 
 })
@@ -5,9 +7,23 @@ new WOW().init();
 
 menuIcon = document.querySelector('.menu-icon')
 mainNav = document.querySelector('.main-navigation')
+if (menuIcon) {
+    menuIcon.addEventListener('click', (event) => {
+        menuIcon.classList.toggle('open')
+        mainNav.classList.toggle('show')
+        document.querySelector('body').classList.toggle('no-scroll')
+    })
+}
 
-menuIcon.addEventListener('click', (event) => {
-    menuIcon.classList.toggle('open')
-    mainNav.classList.toggle('show')
-    document.querySelector('body').classList.toggle('no-scroll')
-})
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    autoplay: true,
+    freeMode: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
--- src/projects/example-project.md
+++
@@ -1,7 +0,0 @@
----
-title: example-project
-caption:
-description: lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-image:
-tags: [featured, projects]
----
