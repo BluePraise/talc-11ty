b73329bb1e0268476f8891aad4a68cea74fa02a0
BluePraise <email@magaliechetrit.com>
2023-12-02 20:35:22 -0500

    reverted to old swiper mark up and JS

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,30 +8,15 @@ if (menuIcon) {
     })
 }
 
-// credit: https://github.com/nolimits4web/swiper/issues/3736
-let swiperCarousels = document.querySelectorAll(".project-slider")
-if (swiperCarousels.length) {
-    let index = 0
-    swiperCarousels.forEach( swiperCarousel => {
-        index = index + 1
+const swiper = new Swiper('.project-slider', {
+    // Optional parameters
+    loop: true,
+    autoplay: true,
+    freeMode: true,
 
-        const swiperPrevButton =
-            swiperCarousel.querySelector(".swiper-prev") ||
-            swiperCarousel.parentElement.querySelector(".swiper-prev")
-
-        const swiperNextButton =
-            swiperCarousel.querySelector(".swiper-next") ||
-            swiperCarousel.parentElement.querySelector(".swiper-next")
-
-        const swiperInstance = new Swiper(swiperCarousel, {
-            loop: true,
-            autoplay: true,
-            freeMode: true,
-
-            navigation: {
-                nextEl: swiperNextButton,
-                prevEl: swiperPrevButton
-            }
-        })
-    })
-}
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-next',
+        prevEl: '.swiper-prev',
+    }
+});
