81d932aac10476d9abdc9e9d93270bd30c7d984c
BluePraise <email@magaliechetrit.com>
2023-12-13 09:39:29 -0500

    removed unused code

--- .eleventy.js
+++ .eleventy.js
@@ -4,15 +4,16 @@ const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+        const jsDate = DateTime.fromJSDate(dateObj)
+        return jsDate.toFormat('dd.ll.yyyy')
     })
-    // let url = "https://api.github.com/repos/11ty/eleventy";
+
     eleventyConfig.setServerOptions({ showVersion: true })
+
     // move css and jpg to output folder
     eleventyConfig.addPassthroughCopy("src/assets/")
     eleventyConfig.addWatchTarget("src/assets/css/*.css")
     eleventyConfig.addPassthroughCopy("src/css/")
-
 
     // presents current year
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
